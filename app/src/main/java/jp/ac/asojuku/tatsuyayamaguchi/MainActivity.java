0e333b5865025001e1caee11b0cae77e29389e23
murasato <tmurasato1601145@gmail.cpm>
2018-10-23 12:47:24 +0900

    画面IDふってます4

--- app/src/main/java/jp/ac/asojuku/tatsuyayamaguchi/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/tatsuyayamaguchi/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        unko
 /*<<<<<<< HEAD
         setContentView(R.layout.activity_main);kk
 =======
