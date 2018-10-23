ac9924bcb085ba6bf422835090ee3e02812c1724
murasato <tmurasato1601145@gmail.cpm>
2018-10-23 14:12:10 +0900

    高須

--- app/src/main/java/jp/ac/asojuku/tatsuyayamaguchi/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/tatsuyayamaguchi/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        iasss
-                unko
+
                 
 /*<<<<<<< HEAD
         setContentView(R.layout.activity_main);kk
