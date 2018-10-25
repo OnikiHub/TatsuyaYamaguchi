9cc0fd2438c91b856429359cc74819f27c24070c
OnikiHub <oniki3030@gmail.com>
2018-10-25 14:18:11 +0900

    ferfefいいうふいh

--- app/src/main/java/jp/ac/asojuku/tatsuyayamaguchi/MySqlConnect.java
+++ app/src/main/java/jp/ac/asojuku/tatsuyayamaguchi/MySqlConnect.java
@@ -1,4 +1,15 @@
 package jp.ac.asojuku.tatsuyayamaguchi;
 
+import java.sql.*;
+
 public class MySqlConnect {
+    static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        String url = "http://jousen5.aso-abcc.com/phpMyAdmin/";
+        String user = "matsuo_user";
+        String pass = "1111";
+        Connection con = DriverManager.getConnection(url,user,pass);
+        return con;
+    }
 }
