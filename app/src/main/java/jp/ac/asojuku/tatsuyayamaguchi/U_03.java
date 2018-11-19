7a0525ed7aea08b21a22ee1cfb8487bc91348c10
God-Matsuo <matsuohibiki1601142@gmail.com>
2018-11-19 11:46:55 +0900

    Default

--- app/src/main/java/jp/ac/asojuku/tatsuyayamaguchi/U_03.java
+++ app/src/main/java/jp/ac/asojuku/tatsuyayamaguchi/U_03.java
@@ -2,7 +2,12 @@ package jp.ac.asojuku.tatsuyayamaguchi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static jp.ac.asojuku.tatsuyayamaguchi.R.id.editTextEmail;
 
 public class U_03 extends AppCompatActivity {
 
@@ -15,7 +20,24 @@ public class U_03 extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        dbm = new DBManager(this);
+        sqlDB = dbm.getWritableDatabase();
 
+        final EditText name = (EditText)findViewById(R.id.editTextName);
+        final EditText tel = (EditText)findViewById(R.id.editTextTEL);
+        final EditText taiju = (EditText)findViewById(R.id.editTextTaiju);
+        final EditText email = (EditText)findViewById(R.id.editTextEmail);
+        Button buttonInsert = (Button)findViewById(R.id.buttonInsert);
 
-    }
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = word.getText().toString();
+
+                if (message != null)dbm.insertWord(sqlDB,message);
+
+                word.setText("");
+                Toast.makeText(getApplicationContext(),"コトバを覚えました", Toast.LENGTH_LONG).show();
+            }
+        });
 }
