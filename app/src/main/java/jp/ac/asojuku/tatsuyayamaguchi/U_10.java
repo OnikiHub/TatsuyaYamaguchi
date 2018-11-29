29c722b3290b71175fb35bc950ccd836e5f831a2
OnikiHub <oniki3030@gmail.com>
2018-11-29 13:55:48 +0900

    顔

--- app/src/main/java/jp/ac/asojuku/tatsuyayamaguchi/U_10.java
+++ app/src/main/java/jp/ac/asojuku/tatsuyayamaguchi/U_10.java
@@ -1,7 +1,9 @@
 package jp.ac.asojuku.tatsuyayamaguchi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class U_10 extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class U_10 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u_10);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+        String result = intent.getStringExtra("result");
+
+        TextView textView = findViewById(R.id.textView8);
+        textView.setText(result);
+
     }
 }
